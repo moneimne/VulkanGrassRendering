7f51dc44d96e2a1ac7a0a602835519a2618f6273
Mohamad Moneimne <moneimne@seas.upenn.edu>
2017-10-01 15:13:27 -0400

    Proper grass blade generation on CPU

--- VulkanGrassRendering/VulkanGrassRendering/Blades.cpp
+++ VulkanGrassRendering/VulkanGrassRendering/Blades.cpp
@@ -1,4 +1,5 @@
 #include "Blades.h"
+#include "Utilities.h"
 
 Blades::Blades() {
 }
@@ -12,17 +13,27 @@ Blades::Blades(Model *model, int numberOfBlades) {
 	for (int i = 0; i < numberOfBlades; i++) {
 		Blade currentBlade = Blade();
 
-		// Generate positions
+		glm::vec3 bladeUp(0.0f, 1.0f, 0.0f);
+
+		// Generate positions and direction (v0)
 		float x = generateRandomFloat() - 0.5f;
 		float y = 0.0f;
 		float z = generateRandomFloat() - 0.5f;
+		float direction = generateRandomFloat() * TWO_PI;
+		glm::vec3 bladePosition(x, y, z);
+		currentBlade.v0 = glm::vec4(bladePosition, direction);
 
-		currentBlade.v0 = glm::vec4(x, y, z, 0.01f);
+		// Bezier point and height (v1)
+		float height = MIN_HEIGHT + (generateRandomFloat() * (MAX_HEIGHT - MIN_HEIGHT));
+		currentBlade.v1 = glm::vec4(bladePosition + bladeUp * height, height);
 
-		// The rest...
-		currentBlade.v1 = glm::vec4(0.0f);
-		currentBlade.v2 = glm::vec4(0.0f);
-		currentBlade.up = glm::vec4(0.0f);
+		// Physical model guide and width (v2)
+		float width = MIN_WIDTH + (generateRandomFloat() * (MAX_WIDTH - MIN_WIDTH));
+		currentBlade.v2 = glm::vec4(bladePosition + bladeUp * height, width);
+
+		// Up vector and stiffness coefficient (up)
+		float stiffness = MIN_BEND + (generateRandomFloat() * (MAX_BEND - MIN_BEND));
+		currentBlade.up = glm::vec4(bladeUp, stiffness);
 
 		blades.push_back(currentBlade);
 	}
--- VulkanGrassRendering/VulkanGrassRendering/Blades.h
+++ VulkanGrassRendering/VulkanGrassRendering/Blades.h
@@ -7,15 +7,21 @@
 #include "Model.h"
 
 const int NUM_BLADES = 512;
+const float MIN_HEIGHT = 1.3f;
+const float MAX_HEIGHT = 2.5f;
+const float MIN_WIDTH = 0.1f;
+const float MAX_WIDTH = 0.14f;
+const float MIN_BEND = 0.5f;
+const float MAX_BEND = 0.7f;
 
 struct Blade {
-	// Position and width
+	// Position and direction
 	glm::vec4 v0;
-	// Bezier point and stiffness coefficient
+	// Bezier point and height
 	glm::vec4 v1;
-	// Physical model guide and direction
+	// Physical model guide and width
 	glm::vec4 v2;
-	// Up vector and height
+	// Up vector and stiffness coefficient
 	glm::vec4 up;
 };
 
